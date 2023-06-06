857058ad8f4e629006406a5ed5a11cb4229624aa
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-06-06 15:26:29 +0300

    Fix date_of_birth upper limit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth >= '2016-1-1' AND date_of_birth <= '2018-1-1';
+SELECT * FROM animals WHERE date_of_birth >= '2016-1-1' AND date_of_birth <= '2019-1-1';
 SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT name, date_of_birth FROM animals where name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
